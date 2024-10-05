17eb73af85f6e0ffb67fc6b28a368aeadcf5768f
vsotreshko <vsotreshko@gmail.com>
2024-10-05 18:14:49 +0200

    feat: Run Bybit

--- start-bot.user.js
+++ start-bot.user.js
@@ -2,7 +2,7 @@
 // @name        Start bot
 // @namespace   Violentmonkey Scripts
 // @grant       none
-// @version     3.2
+// @version     3.3
 // @author      -
 // @description 9/1/2024, 7:13:21 PM
 // @match       *://web.telegram.org/*
