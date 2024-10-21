b3ad06be9ac49eb6e7755ac8a2daa0f472f8fd18
vsotreshko <vsotreshko@gmail.com>
2024-10-21 18:32:58 +0200

    feat(major): Created Major script

---
+++ major.user.js
@@ -0,0 +1,101 @@
+// ==UserScript==
+// @name        Major
+// @namespace   Violentmonkey Scripts
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 9/1/2024, 7:13:21 PM
+// @match       *://major.bot/*
+// @downloadURL https://github.com/vsotreshko/bug-free-happiness/raw/main/major.user.js
+// @updateURL   https://github.com/vsotreshko/bug-free-happiness/raw/main/major.user.js
+// ==/UserScript==
+
+/** Custom functions -------------------------------------------------------------- */
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+const simulateClick = (button) => {
+  // Simulate a real mouse click on the launchBotButton
+  const rect = button.getBoundingClientRect();
+  const centerX = rect.left + rect.width / 2;
+  const centerY = rect.top + rect.height / 2;
+
+  button.dispatchEvent(
+    new MouseEvent("mousedown", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: centerX,
+      clientY: centerY,
+    })
+  );
+
+  button.dispatchEvent(
+    new MouseEvent("mouseup", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: centerX,
+      clientY: centerY,
+    })
+  );
+
+  button.dispatchEvent(
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: centerX,
+      clientY: centerY,
+    })
+  );
+
+  button.click();
+};
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const waitForElement = async (document, selector, timeout = 10000) => {
+  console.warn(`Waiting for: ${selector}`);
+  return new Promise((resolve, reject) => {
+    if (document.querySelector(selector)) {
+      console.warn(`waitForElement: found ${selector} immediately`);
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        console.warn(`waitForElement: found ${selector} after DOM changed`);
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    // Resolve promise after 10 seconds if element is not found
+    setTimeout(() => {
+      console.warn(`waitForElement: ${selector} not found after 10 seconds`);
+      observer.disconnect();
+      resolve(null);
+    }, timeout);
+  });
+};
+
+/** ------------------------------------------------------------------------------- */
+
+const init = async () => {
+  window.onclick = (e) => {
+    console.log(e.target); // to get the element
+    console.log(e.target.tagName); // to get the element tag name alone
+  };
+
+  console.log("MAJOR Running...");
+};
+
+init();
