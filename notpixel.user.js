515daa4186869300c6d6b606164f025295fef5cc
vsotreshko <vsotreshko@gmail.com>
2024-11-06 17:16:56 +0100

    feat(): Updated scripts

--- notpixel.user.js
+++ notpixel.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        NotPixel
+// @name        Notpixel
 // @namespace   Violentmonkey Scripts
 // @grant       none
 // @version     1.0
@@ -10,6 +10,7 @@
 // @updateURL    https://github.com/vsotreshko/bug-free-happiness/raw/main/notpixel.user.js
 // ==/UserScript==
 
+// !!! NOT PIXEL CODE !!!
 /** Custom functions -------------------------------------------------------------- */
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
@@ -121,6 +122,38 @@ const simulateClickX = (button) => {
   });
 };
 
+const simulateClickHaloween = (button) => {
+  const events = [
+    new PointerEvent("pointerdown", {
+      bubbles: true,
+      cancelable: true,
+      isTrusted: true,
+      pointerId: 1,
+      width: 1,
+      height: 1,
+      pressure: 0.5,
+      pointerType: "touch",
+    }),
+    new MouseEvent("mousedown", { bubbles: true, cancelable: true, isTrusted: true, offsetX: 137, offsetY: 91 }),
+    new PointerEvent("pointerup", {
+      bubbles: true,
+      cancelable: true,
+      isTrusted: true,
+      pointerId: 1,
+      width: 1,
+      height: 1,
+      pressure: 0,
+      pointerType: "touch",
+    }),
+    new MouseEvent("mouseup", { bubbles: true, cancelable: true, isTrusted: true, offsetX: 137, offsetY: 91 }),
+    new PointerEvent("click", { bubbles: true, cancelable: true, isTrusted: true, offsetX: 137, offsetY: 91 }),
+  ];
+
+  events.forEach((event, index) => {
+    setTimeout(() => button.dispatchEvent(event), index * 100);
+  });
+};
+
 const waitForElement = async (document, selector, timeout = 10000) => {
   console.warn(`Waiting for: ${selector}`);
   return new Promise((resolve, reject) => {
@@ -143,7 +176,7 @@ const waitForElement = async (document, selector, timeout = 10000) => {
 
     // Resolve promise after 10 seconds if element is not found
     setTimeout(() => {
-      console.warn(`waitForElement: ${selector} not found after 10 seconds`);
+      console.warn(`waitForElement: ${selector} not found after ${timeout} ms`);
       observer.disconnect();
       resolve(null);
     }, timeout);
@@ -169,13 +202,14 @@ function simulatePointerEvents(element, startX, startY, endX, endY) {
 }
 
 const clickPaintButton = async (document) => {
-  const paintButton = await waitForElement(document, "#root > div > div:nth-child(7) > div > button > span");
+  const paintButton = await waitForElement(document, "#root > div > div._order_panel_lwgvy_1 > div > button > span");
   simulateClickX(paintButton);
 };
 
 const findColors = async (document) => {
   // Click on the active color
-  const activeColorSelector = "#root > div > div:nth-child(7) > div > div:nth-child(2) > div:nth-child(1)";
+  const activeColorSelector =
+    "#root > div > div._order_panel_lwgvy_1 > div > div._info_lwgvy_42 > div._active_color_lwgvy_51";
   const activeColor = await waitForElement(document, activeColorSelector);
   simulateClickX(activeColor);
 
@@ -183,7 +217,8 @@ const findColors = async (document) => {
   await delay(1000);
 
   // Get palette colors
-  const paletteSelector = "#root > div > div:nth-child(7) > div > div:nth-child(3) > div > div:nth-child(2)";
+  const paletteSelector =
+    "#root > div > div._order_panel_lwgvy_1 > div > div._expandable_panel_layout_1v9vd_1 > div > div._color_line_epppt_15";
   const palette = await waitForElement(document, paletteSelector);
 
   const colors = [];
@@ -225,9 +260,52 @@ const selectBlumTemplate = async (document) => {
 
 const getCanPaintCount = async (document) => {
   const canPaintCountSelector =
-    "#root > div > div:nth-child(7) > div > button > div:nth-child(1) > div > div:nth-child(2) > span:nth-child(2)";
+    "#root > div > div:nth-child(8) > div > button > div:nth-child(1) > div > div:nth-child(2) > span:nth-child(2)";
   const canPaintCountElement = await waitForElement(document, canPaintCountSelector);
   return parseInt(canPaintCountElement.textContent);
+};
+
+const autoClaimReward = async (document, toClose = true) => {
+  const claimMenuButtonSelector = "#root > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(2) > button";
+  const claimMenuButton = await waitForElement(document, claimMenuButtonSelector);
+  simulateClick(claimMenuButton);
+
+  await delay(1000);
+
+  const claimButtonSelector =
+    "#root > div > div._layout_4nkxd_1 > div._content_4nkxd_22 > div._container_13oyr_1 > button";
+  const claimButton = await waitForElement(document, claimButtonSelector);
+  simulateClick(claimButton);
+
+  await delay(1000);
+
+  if (toClose) {
+    const backButtonSelector = "#root > div > div:nth-child(2) > button";
+    const backButton = await waitForElement(document, backButtonSelector);
+    simulateClick(backButton);
+  }
+};
+
+const resolveTasks = async (document) => {
+  const taskRowSelector =
+    "#root > div > div._layout_4nkxd_1 > div._content_4nkxd_22 > div._info_layout_bt2qf_1 > div > div:nth-child(1) > div";
+  const taskButton = await waitForElement(document, taskRowSelector);
+  simulateClick(taskButton);
+
+  await delay(1000);
+
+  const symbolTaskSelector =
+    "#root > div > div._layout_4nkxd_1 > div._content_4nkxd_22 > div._info_layout_bt2qf_1 > div > div:nth-child(9) > div";
+  const symbolTask = await waitForElement(document, symbolTaskSelector);
+  simulateClick(symbolTask);
+
+  await delay(1000);
+
+  const checkButtonSelector = "body > div._popup_1ub07_11 > div > div:nth-child(4) > button";
+  const checkButton = await waitForElement(document, checkButtonSelector);
+  simulateClick(checkButton);
+
+  console.log("Task resolved");
 };
 
 const init = async () => {
@@ -242,14 +320,38 @@ const init = async () => {
     return;
   }
 
+  await delay(2000);
+
+  const templateCatalogTitleSelector = "#root > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > h1";
+  const templateCatalogTitle = await waitForElement(document, templateCatalogTitleSelector, 2000);
+
+  if (templateCatalogTitle && templateCatalogTitle.textContent.trim().toLowerCase().includes("templates")) {
+    await delay(2000);
+
+    const blumTemplateSelector =
+      "#root > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(1) > div:nth-child(2) > div > img";
+    const blumTemplate = await waitForElement(document, blumTemplateSelector);
+    simulateClick(blumTemplate);
+
+    await delay(1000);
+
+    const buttonSelector = "body > div:nth-child(8) > div > div > div > div:nth-child(4) > button";
+    const button = await waitForElement(document, buttonSelector);
+    simulateClick(button);
+
+    await delay(2000);
+
+    window.location.reload();
+  }
+
   const canvas = await waitForElement(document, "#canvasHolder");
   await changeCursorPositionOnCanvas(canvas);
   await delay(1000);
 
   const canPaintCount = await getCanPaintCount(document);
 
-  if (canPaintCount > 0 && canPaintCount % 2 === 0) {
-    for (let i = 0; i < canPaintCount / 2; i++) {
+  if (canPaintCount > 1) {
+    for (let i = 0; i < Math.floor(canPaintCount / 2); i++) {
       await delay(1000);
       await changeCursorPositionOnCanvas(canvas);
       const colors = await findColors(document);
@@ -262,6 +364,10 @@ const init = async () => {
       }
     }
   }
+
+  await autoClaimReward(document, false);
+
+  await resolveTasks(document);
 };
 
 init();
---
+++ paws.user.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name        Paws
+// @namespace   Violentmonkey Scripts
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 9/1/2024, 7:13:21 PM
+// @match        *://*paws.community/*
+// @downloadURL  https://github.com/vsotreshko/bug-free-happiness/raw/main/paws.user.js
+// @updateURL    https://github.com/vsotreshko/bug-free-happiness/raw/main/paws.user.js
+// ==/UserScript==
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+const waitForElement = async (document, selector) => {
+  console.warn(`Waiting for: ${selector}`);
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      console.warn(`waitForElement: found ${selector} immediately`);
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        console.warn(`waitForElement: found ${selector} after DOM changed`);
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    // Resolve promise after 10 seconds if element is not found
+    setTimeout(() => {
+      console.warn(`waitForElement: ${selector} not found after 10 seconds`);
+      observer.disconnect();
+      resolve(null);
+    }, 10000);
+  });
+};
+
+const iterateOverTasks = async (document) => {
+  const dontResolve = ["connect wallet", "invite 10 friends", "boost paws channel", "follow channel"];
+
+  const questsListSelector =
+    "#next-app > div.main-page-con > div.main-content-container.is-show > div.main-content-wrapper > div.quests-tab-con.is-show > div > div.section-items-con.quests";
+  const questsList = await waitForElement(document, questsListSelector);
+
+  const elements = questsList.querySelectorAll(".invite-item");
+  for (const quest of elements) {
+    const text = quest.querySelector(".wallet");
+    const shouldntProcess = dontResolve.find((code) => code === text.textContent.trim().toLowerCase());
+    if (!shouldntProcess) {
+      const button = quest.querySelector(".start-btn");
+      if (button) {
+        await delay(2000);
+        button.click();
+      }
+    }
+  }
+};
+
+const resolveEarn = async (document) => {
+  console.log("Resolving earn");
+
+  const earnTabSelector =
+    "#next-app > div.main-page-con > div.main-content-container.is-show > div.nav-bar-con > div:nth-child(4)";
+  const earnTab = await waitForElement(document, earnTabSelector);
+  earnTab.click();
+
+  await iterateOverTasks(document); // Start
+  await iterateOverTasks(document); // Check
+  await iterateOverTasks(document); // Claim
+
+  const partnersTabSelector =
+    "#next-app > div.main-page-con > div.main-content-container.is-show > div.main-content-wrapper > div.quests-tab-con.is-show > div > div.type-select > div:nth-child(2)";
+  const partnersTab = await waitForElement(document, partnersTabSelector);
+  partnersTab.click();
+
+  await iterateOverTasks(document); // Start
+  await iterateOverTasks(document); // Check
+  await iterateOverTasks(document); // Claim
+};
+
+const init = async () => {
+  await delay(10000);
+
+  await resolveEarn(document);
+};
+
+init();
